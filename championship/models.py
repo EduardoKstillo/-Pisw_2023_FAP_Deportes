cb62dbf0728a4e7de6a56ce03dec064bfba83a96
DeysonDiaz <ddiazti@unsa.edu.pe>
2023-10-11 16:17:24 -0500

    adding field img in model persons

--- championship/models.py
+++ championship/models.py
@@ -67,6 +67,7 @@ class Person(models.Model):
     promotion_sub_delegate = models.BooleanField(default=False)
     partner = models.BooleanField(default=False)
     NSA_code = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(upload_to='pics')
 
     def __str__(self):
         return self.name
