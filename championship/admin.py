4cbf2f13c36cb897db5d057bbbf5f3e7e255f694
BRAYAN EMILIO GARCIA RENGIFO <bgarcia@unsa.edu.pe>
2023-10-17 01:59:03 +0000

    Update 63 files
    - /championship/migrations/0001_initial.py
    - /championship/migrations/__init__.py
    - /championship/__init__.py
    - /championship/admin.py
    - /championship/apps.py
    - /championship/filters.py
    - /championship/fixture.py
    - /championship/forms.py
    - /championship/models.py
    - /championship/tests.py
    - /championship/urls.py
    - /championship/views.py
    - /core/__init__.py
    - /core/asgi.py
    - /core/settings.py
    - /core/urls.py
    - /core/wsgi.py
    - /partner/migrations/0001_initial.py
    - /partner/migrations/0002_alter_partner_activity_alter_partner_address_and_more.py
    - /partner/migrations/__init__.py
    - /partner/__init__.py
    - /partner/admin.py
    - /partner/apps.py
    - /partner/decorators.py
    - /partner/filters.py
    - /partner/forms.py
    - /partner/models.py
    - /partner/tests.py
    - /partner/urls.py
    - /partner/validators.py
    - /partner/views.py
    - /static/css/index.css
    - /static/img/fap.svg
    - /static/javascript/messages.js
    - /static/javascript/sidebar.js
    - /templates/layout/footer.html
    - /templates/layout/header.html
    - /templates/layout/main.html
    - /templates/layout/sidebar.html
    - /templates/championship/category/categorys.html
    - /templates/championship/category/create_category.html
    - /templates/championship/championship/championship.html
    - /templates/championship/championship/create_championship.html
    - /templates/championship/championship/view_championship.html
    - /templates/championship/player/create_player.html
    - /templates/championship/player/players.html
    - /templates/championship/team/create_team.html
    - /templates/championship/team/teams.html
    - /templates/users/partner/create_partner.html
    - /templates/users/partner/detail_partner.html
    - /templates/users/partner/details_partner.html
    - /templates/users/partner/edit_partner.html
    - /templates/users/partner/partner.html
    - /templates/users/user/create_user.html
    - /templates/users/user/edit_user.html
    - /templates/users/user/users.html
    - /templates/championship/fixture.html
    - /templates/championship/games.html
    - /templates/index.html
    - /templates/login.html
    - /.gitignore
    - /config.txt
    - /manage.py

--- championship/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Category, Championship, Team, Player
-
-admin.site.register(Category)
-admin.site.register(Championship)
-admin.site.register(Team)
-admin.site.register(Player)
